4c91d651ab8929f4196e3054aa12e44cf4aaeb40
Maksim <savkin.mk@phystech.edu>
2022-05-13 20:45:12 +0300

    basic tests

--- tests.py
+++ tests.py
@@ -1,10 +1,41 @@
-from src.model import generate_answer
+import requests
+import pytest
 
-print(f"Everything works fine! Model output: {generate_answer('Who are you?')}")
+
+URL = "http://127.0.0.1:8888/"
+
+def make_request(question: str) -> str:
+    res = requests.post(URL, json={"question": question})
+    return res.json()["answer"]
 
 
-#  import requests
+def test_model_answers():
+        input_questions = [
+            "What is the capital of Russia?",
+            "What is the capital of Great Britain?",
+            "How many countries are there?",
+            "What is natural language processing?",
+            "How many plays did Shakespeare write?",
+            "Who is Winston Churchill?",
+            "How did Baromir die?",
+            "What is love?",
+            "What was the name of Dumbledore's bird from Harry Potter?"
+        ]
 
-# answer = requests.post("http://127.0.0.1:8888/", "42").request
+        gold_answers = [
+            "Moscow",
+            "London",
+            "120",
+            "a subfield of computer science, information engineering, and artificial intelligence",
+            "Three",
+            "unknown",
+            "unconscious",
+            "to will the good of another",
+            "Professor Albus Percival Wulfric Brian"
+        ]
 
-# print(answer)
+        for question, answer in zip(input_questions, gold_answers):
+            assert(make_request(question).lower() == answer.lower())
+
+if __name__ == '__main__':
+    test_model_answers()
